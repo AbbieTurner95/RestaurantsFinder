7b94ca9a1b25617051bbb5d51ad328d21047920a
Abbieturner95 <abbieturner95@hotmail.com>
2018-11-12 19:12:13 +0000

    added internet and network perms
    also added restaurant json adapter to get json from json object

---
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Adapters/RestaurantJsonAdapter.java
@@ -0,0 +1,19 @@
+package com.example.abbieturner.restaurantsfinder.Adapters;
+
+
+import com.example.abbieturner.restaurantsfinder.Data.Restaurant;
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+public class RestaurantJsonAdapter implements JsonDeserializer<Restaurant> {
+    @Override
+    public Restaurant deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonElement jsonRestaurant = json.getAsJsonObject().get("restaurant");
+        return new Gson().fromJson(jsonRestaurant, Restaurant.class);
+    }
+}
