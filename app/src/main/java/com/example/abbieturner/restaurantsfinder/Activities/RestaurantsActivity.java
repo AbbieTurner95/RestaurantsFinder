cc2f76201202e0aaa5b57c551332cca081579d9d
Abbieturner95 <abbieturner95@hotmail.com>
2018-11-16 11:37:06 +0000

    pulled data from API for restaurants

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Activities/RestaurantsActivity.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Activities/RestaurantsActivity.java
@@ -1,9 +1,6 @@
 package com.example.abbieturner.restaurantsfinder.Activities;
 
-import android.app.ActivityOptions;
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,27 +8,34 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.abbieturner.restaurantsfinder.API.API;
+import com.example.abbieturner.restaurantsfinder.Adapters.CuisineJsonAdapter;
 import com.example.abbieturner.restaurantsfinder.Adapters.RestaurantsAdapter;
+import com.example.abbieturner.restaurantsfinder.Data.Cuisine;
 import com.example.abbieturner.restaurantsfinder.Data.Restaurant;
+import com.example.abbieturner.restaurantsfinder.Data.Restaurants;
 import com.example.abbieturner.restaurantsfinder.R;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class RestaurantsActivity extends AppCompatActivity implements RestaurantsAdapter.RestaurantItemClick {
 
-
-
-
-    private Intent intent;
-    private String name, id;
+    private String name;
+    private int cuisineID;
     RecyclerView recyclerView;
     private RestaurantsAdapter restaurantsAdapter;
-    private LinearLayoutManager layoutManager;
+    private API.ZomatoApiCalls service;
 
 
     @Override
@@ -40,47 +44,57 @@ public class RestaurantsActivity extends AppCompatActivity implements Restaurant
         setContentView(R.layout.activity_restaurants);
 
         recyclerView = findViewById(R.id.restaurants_recycler_view);
-        layoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         restaurantsAdapter = new RestaurantsAdapter(this, this);
         recyclerView.setAdapter(restaurantsAdapter);
 
-
-        intent = getIntent();
+        Intent intent = getIntent();
 
         if(intent != null){
-            id = intent.getStringExtra(getResources().getString(R.string.TAG_CUISINE_ID));
+            cuisineID = Integer.parseInt(intent.getStringExtra(getResources().getString(R.string.TAG_CUISINE_ID)));
             name = intent.getStringExtra(getResources().getString(R.string.TAG_CUISINE_NAME));
         } else {
-            Log.e("ERROR INTENT", "Intent null");
+            Log.e("ERROR INTENT", "Intent is null!");
         }
 
         this.setTitle(name);
 
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Cuisine.class, new CuisineJsonAdapter())
+                .create();
 
+        final String BASE_URL = getResources().getString(R.string.BASE_URL_API);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
 
-        restaurantsAdapter.setRestaurantsList(setMockData());
+        service = retrofit.create(API.ZomatoApiCalls.class);
+
+        fetchRestaurants();
     }
+
+    private void fetchRestaurants(){
+
+        service.getRestaurants("332", "city", "1", "20",
+                "53.382882", "-1.470300", "rating", cuisineID, "asc")
+                .enqueue(new Callback<Restaurants>() {
+                    @Override
+                    public void onResponse(Call<Restaurants> call, Response<Restaurants> response) {
+                        assert response.body() != null;
+                        restaurantsAdapter.setRestaurantsList(response.body().restaurantList);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Restaurants> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
+        }
 
     @Override
     public void onRestaurantItemClick(Restaurant restaurant){
-
-
-        Toast.makeText(this, restaurant.getName(),
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(this, restaurant.getName(), Toast.LENGTH_LONG).show();
     }
-
-    private List<Restaurant> setMockData(){
-        List<Restaurant> myList = new ArrayList<>();
-
-        myList.add(new Restaurant("1", "Restaurant 1"));
-        myList.add(new Restaurant("2", "Restaurant 2"));
-        myList.add(new Restaurant("3", "Restaurant 3"));
-        myList.add(new Restaurant("4", "Restaurant 4"));
-        myList.add(new Restaurant("5", "Restaurant 5"));
-
-
-        return myList;
-    }
-
 }
