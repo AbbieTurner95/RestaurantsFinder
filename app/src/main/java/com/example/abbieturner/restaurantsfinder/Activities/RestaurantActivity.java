46b5921d549a5cf9daa8300bce384d75d38570b9
Abbieturner95 <abbieturner95@hotmail.com>
2018-11-28 11:56:40 +0000

    changed UI + added horizontal slider for reviews

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Activities/RestaurantActivity.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Activities/RestaurantActivity.java
@@ -2,16 +2,30 @@ package com.example.abbieturner.restaurantsfinder.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.abbieturner.restaurantsfinder.Database.AppDatabase;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import com.example.abbieturner.restaurantsfinder.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RestaurantActivity extends AppCompatActivity {
+
+    @BindView(R.id.review_slider)
+    RecyclerView slider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
+        ButterKnife.bind(this);
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        slider.setLayoutManager(layoutManager);
+        //needs adapter
+
+
     }
 
 }
