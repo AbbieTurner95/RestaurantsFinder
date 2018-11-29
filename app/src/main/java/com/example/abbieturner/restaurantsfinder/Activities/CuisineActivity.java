618decf3cdc0bc5dea19f061a4bcaeb85d85dca1
Abbieturner95 <abbieturner95@hotmail.com>
2018-11-29 12:38:14 +0000

    update

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Activities/CuisineActivity.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Activities/CuisineActivity.java
@@ -2,13 +2,19 @@ package com.example.abbieturner.restaurantsfinder.Activities;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.transition.Fade;
+import android.view.MenuItem;
 
 import com.example.abbieturner.restaurantsfinder.API.API;
 import com.example.abbieturner.restaurantsfinder.Adapters.CuisineAdapter;
