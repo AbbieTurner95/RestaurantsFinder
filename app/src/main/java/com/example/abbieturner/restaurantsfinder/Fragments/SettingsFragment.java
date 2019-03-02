d6f395aeda869c032169ebf08ae2d63f7a0314f9
Abbieturner95 <abbieturner95@hotmail.com>
2019-03-02 19:20:01 +0000

    update

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Fragments/SettingsFragment.java
@@ -1,7 +1,9 @@
 package com.example.abbieturner.restaurantsfinder.Fragments;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.SwitchPreferenceCompat;
 
 import com.example.abbieturner.restaurantsfinder.R;
 
@@ -10,4 +12,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings_pref);
     }
+
+    //boolean GPS = ((SwitchPreferenceCompat) findPreference(PreferenceManager.REMOTE_ACCESS_ACTIVE)).isChecked();
+
+
+
 }
