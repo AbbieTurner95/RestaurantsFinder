fa6676ae9bc5b37f1c89e41a335e2d9533e51859
Abbieturner95 <abbieturner95@hotmail.com>
2019-03-04 11:07:36 +0000

    widget works (only opens app so far)

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Widget/RestaurantWidgetProvider.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Widget/RestaurantWidgetProvider.java
@@ -1,23 +1,36 @@
 package com.example.abbieturner.restaurantsfinder.Widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.example.abbieturner.restaurantsfinder.Activities.RestaurantsActivity;
 import com.example.abbieturner.restaurantsfinder.R;
 
-public class RestaurantWidget extends AppWidgetProvider {
+public class RestaurantWidgetProvider extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.restaurant_widget);
+        RemoteViews views = new RemoteViews(context.getPackageName(),
+                                    R.layout.restaurant_widget_provider);
+
+        Intent intent = new Intent(context, RestaurantsActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.restaurant_icon_widget, pendingIntent);
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
     }
 
     @Override
