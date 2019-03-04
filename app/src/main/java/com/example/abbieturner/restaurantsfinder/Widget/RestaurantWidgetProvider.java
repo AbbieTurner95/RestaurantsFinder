08aedc77bb8d10e35bbd3389595468bf41e1a05f
Abbieturner95 <abbieturner95@hotmail.com>
2019-03-04 11:41:03 +0000

    widget (takes to cuisine list)

--- app/src/main/java/com/example/abbieturner/restaurantsfinder/Widget/RestaurantWidgetProvider.java
+++ app/src/main/java/com/example/abbieturner/restaurantsfinder/Widget/RestaurantWidgetProvider.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.example.abbieturner.restaurantsfinder.Activities.CuisineActivity;
+import com.example.abbieturner.restaurantsfinder.Activities.FavouritesActivity;
 import com.example.abbieturner.restaurantsfinder.Activities.RestaurantsActivity;
 import com.example.abbieturner.restaurantsfinder.R;
 
@@ -18,7 +20,7 @@ public class RestaurantWidgetProvider extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(),
                                     R.layout.restaurant_widget_provider);
 
-        Intent intent = new Intent(context, RestaurantsActivity.class);
+        Intent intent = new Intent(context, CuisineActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
         views.setOnClickPendingIntent(R.id.restaurant_icon_widget, pendingIntent);
